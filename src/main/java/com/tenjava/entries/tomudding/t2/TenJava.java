ece6d1c076e0e220e0577a8d4a45267924ad1d3d
tomudding <tomudding@gmail.com>
2014-07-12 13:26:56 +0200

    Update TenJava.java

--- src/main/java/com/tenjava/entries/tomudding/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/tomudding/t2/TenJava.java
@@ -1,5 +1,23 @@
 package com.tenjava.entries.tomudding.t2;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+import com.tenjava.entries.tomudding.t2.listeners.PlayerListener;
+
+public class TenJava extends JavaPlugin {
+
+    public static TenJava instance;
+    private final PlayerListener listener1 = new PlayerListener(this);
+    
+    public void onEnable() {
+        instance = this;
+        
+        // Register Events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this.listener1, this);
+    }
+
+    public void onDisable() {
+    }
+}
