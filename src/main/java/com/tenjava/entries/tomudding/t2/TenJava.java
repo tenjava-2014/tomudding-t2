8000ed39a45b4c673693484202663c192c9a4ffb
tomudding <tomudding@gmail.com>
2014-07-12 19:29:33 +0200

    Last Update...

--- src/main/java/com/tenjava/entries/tomudding/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/tomudding/t2/TenJava.java
@@ -11,14 +11,20 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.tenjava.entries.tomudding.t2.commands.Command;
+import com.tenjava.entries.tomudding.t2.listeners.FurnaceListener;
+import com.tenjava.entries.tomudding.t2.listeners.MinecartListener;
 import com.tenjava.entries.tomudding.t2.listeners.PlayerListener;
 import com.tenjava.entries.tomudding.t2.managers.ChargeManager;
+import com.tenjava.entries.tomudding.t2.utils.ChatManager;
 
 public class TenJava extends JavaPlugin {
 
     public static TenJava instance;
     private final PlayerListener listener1 = new PlayerListener(this);
-	public ChargeManager settings = ChargeManager.getInstance();
+    public ChargeManager settings = ChargeManager.getInstance();
+    public int ChargeAmount;
+    public int FurnaceAmount;
     
     public void onEnable() {
     	saveDefaultConfig();
@@ -28,16 +34,22 @@ public class TenJava extends JavaPlugin {
         // Register Events
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this.listener1, this);
-        
+
         // Loading Recipe
         orbRecipe();
         
         // Setting Up ChargeManager
-	    this.settings.setup(this);
-	    this.settings.saveData();
+	this.settings.setup(this);
+	this.settings.saveData();
+	    
+	// Loading Config
+	loadConfigVariables();
+	    
+	ChatManager.get().log("ten.java contest plugin by Tomudding is enabled");
     }
 
     public void onDisable() {
+	    ChatManager.get().log("ten.java contest plugin by Tomudding is disabled");
     }
     
     public void orbRecipe() {
@@ -57,4 +69,9 @@ public class TenJava extends JavaPlugin {
   	  orbRecipe.setIngredient('?', Material.MAGMA_CREAM);
   	  Bukkit.getServer().addRecipe(orbRecipe);
     }
+    
+	private void loadConfigVariables() {
+	  this.ChargeAmount = getConfig().getInt("Charge-Units");
+	  this.FurnaceAmount = getConfig().getInt("Furnace-Units");
+	}
 }
